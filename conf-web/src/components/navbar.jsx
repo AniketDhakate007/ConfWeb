ce713302076be701df60bc689bf8bdd16cc52956
ras41 <rs2krs3@gmail.com>
2025-03-07 00:33:13 +0530

    call for paper

--- conf-web/src/components/navbar.jsx
+++ conf-web/src/components/navbar.jsx
@@ -1,24 +1,29 @@
+<<<<<<< Updated upstream
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { Menu, X } from "lucide-react";
+=======
+import { Link } from "react-router-dom";
+import "./navbar.css";
+>>>>>>> Stashed changes
 
-export default function(){
-   
-    return(
-        <div className="navbar flexbox">
-            <div className='nav-one flexbox-spacearound'>
-            <Link to="/">Home</Link>
-            <Link to="/advboard">Advisory Board</Link>
-            <Link to="/commitee">Committees</Link>
-            <Link to="/kspeaker">Keynote Speakers</Link>
-            <Link to="/authors">Call for Papers</Link>
-            <Link to="/register">Registration</Link>
-            <Link to="/program">Programme Schedule</Link>
-            </div>
-            <div className='nav-two flexbox-spacearound'>
-            <Link to="/contact">Contact Us</Link>
-            <Link to="/about">About</Link>
-            </div>
-        </div>
-    )
+export default function () {
+  return (
+    <div className="navbar flexbox">
+      <div className="nav-one flexbox-spacearound">
+        <Link to="/">Home</Link>
+        <Link to="/advboard">Advisory Board</Link>
+        <Link to="/commitee">Committees</Link>
+        <Link to="/kspeaker">Keynote Speakers</Link>
+        <Link to="/call_for_papers">Call for Papers</Link>
+        <Link to="/register">Registration</Link>
+        <Link to="/program">Programme Schedule</Link>
+        <Link to="/call_for_papers">Themes</Link>
+      </div>
+      <div className="nav-two flexbox-spacearound">
+        <Link to="/contact">Contact Us</Link>
+        <Link to="/about">About</Link>
+      </div>
+    </div>
+  );
 }
