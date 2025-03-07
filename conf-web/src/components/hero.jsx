6d38f82cc0777a28aa220b6c3318d205f47ea80a
Ritish Das <ritishdas116@outlook.com>
2025-03-08 04:41:22 +0500

    navbar final

--- conf-web/src/components/hero.jsx
+++ conf-web/src/components/hero.jsx
@@ -1,4 +1,6 @@
+import {useNavigate} from "react-router-dom";
 export default function(){
+  const navigate = useNavigate();
     return(
         <div className="hero flexbox-spacearound">
             <div className="flexbox-column flexbox">
@@ -9,8 +11,8 @@ International Conference on Emerging Trends and Research in Computer & Electroni
 Organized by Department of Computer Science and Engineering & Artificial Intelligence and Data Science, KDKCE ,Nagpur.
             </p>
             <div className="flexbox hero-button">
-                <button>Register</button>
-                <span>Learn more</span>
+                <button onClick={()=>{navigate('/register')}}>Register</button>
+                <span onClick={()=>{navigate('/about')}}>Learn more</span>
             </div>
             </div>
 <h1>
--- conf-web/src/components/navbar.jsx
+++ conf-web/src/components/navbar.jsx
@@ -1,38 +1,35 @@
-import { useState } from "react";
 import { Link } from "react-router-dom";
-import { Menu, X } from "lucide-react";
-// import { Link } from "react-router-dom";
-import "./navbar.css";
 
 export default function () {
   return (
 
     <nav className="navbar">
+    <span>Menu</span>
       <div className="nav-container">
 
-        <div className={`nav-one`}>
+        <div className='nav-one'>
           <Link to="/">Home</Link>
           <Link to="/advboard">Advisory Board</Link>
           <Link to="/commitee">Committees</Link>
           <Link to="/kspeaker">Keynote Speakers</Link>
           <div className="dropdown"><span>For Authors</span>
           <div>
-<Link to="/about">Call for Paper</Link>
+<Link to="/call_for_Papers">Call for Paper</Link>
 <Link to="/about">Important Dates</Link>
-<Link to="/about">Paper Format</Link>
-<Link to="/about">Paper Submission</Link>
+<Link to="/">Paper Format</Link>
+<Link to="/">Paper Submission</Link>
           </div>
           </div>
           <Link to="/register">Registration</Link>
           <Link to="/program">Programme Schedule</Link>
         </div>
 
-        <div className={`nav-two`}>
+        <div className='nav-two'>
           <Link to="/contact">Contact Us</Link>
           <div className="dropdown"><span>About</span>
           <div>
 <Link to="/about">About College</Link>
-<Link to="/about">Theme and Scope</Link>
+<Link to="/Theme-scope">Theme and Scope</Link>
           </div>
           </div>
         </div>
@@ -41,3 +38,9 @@ export default function () {
     </nav>
   );
 }
+/*
+   { name: 'Call For Papers', path: '/call_for_papers' },
+    { name: 'Important Dates', path: '/about' },
+    { name: 'Paper Format', path: '/' },
+    { name: 'Paper Submission', path: '/' }
+ * */
